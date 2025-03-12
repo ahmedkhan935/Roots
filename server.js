20a659a53646f8867ad1e133693a75dbdd1c47d6
sohaib-ahmed869 <mahad112002@g,mail.com>
2025-03-13 03:54:23 +0500

    ok

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const teacherRoutes = require("./routes/TeacherRoute");
 app.use(express.json());
 app.use(
   cors({
-    origin: "*"
+    origin: "https://rgsmeritportal.com"
   })
 );
 app.use("/api/auth", authroutes);
